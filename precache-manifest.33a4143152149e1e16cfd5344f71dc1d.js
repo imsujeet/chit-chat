ca53242701d2dabbf3bb0f4b46e2dcc8ec36a472
Sujeet Thakur <35932820+imsujeet@users.noreply.github.com>
2019-07-20 12:48:19 +0530

    Updates

---
+++ precache-manifest.33a4143152149e1e16cfd5344f71dc1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45cf0150f042fd7f341b22a0109fa518",
+    "url": "/chit-chat/index.html"
+  },
+  {
+    "revision": "acfbd55d321190643b6f",
+    "url": "/chit-chat/static/js/2.4c1856f3.chunk.js"
+  },
+  {
+    "revision": "975d99ffea19a74b6710",
+    "url": "/chit-chat/static/js/main.d71a30b5.chunk.js"
+  },
+  {
+    "revision": "86274b48ba23cc2997b6",
+    "url": "/chit-chat/static/js/runtime~main.1ce857a8.js"
+  }
+]);
